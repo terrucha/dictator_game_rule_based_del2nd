34e7abb07f87e0592c11d9b52ff6687f0dd3d147
DataTeller <151622487+morbifakhar69@users.noreply.github.com>
2025-07-29 00:56:47 +0200

    Update models.py
    added app name

--- dictator_game/models.py
+++ dictator_game/models.py
@@ -32,6 +32,8 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     # Allocation for the current decision (manual or agent-based)
+    app_name = models.StringField(initial='rulebased_del2nd')
+
     allocation = models.IntegerField(
         min=0,
         max=100,
